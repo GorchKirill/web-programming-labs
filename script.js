89081fb7239fc90c12a42896452ffc485910ffac
Студент Факультета Бизнеса <student@FB.LOCAL>
2022-05-26 10:24:11 +0700

    Генерация меню

--- script.js
+++ script.js
@@ -26,14 +26,44 @@ function search() {
         card.style.border = 'none';
         card.style.backgroundColor = '';
     }, 2000);
+}
 
-    function loaded() {
-        let searchbox = document.getElementById('search');
-        searchbox.addEventListener('keydown', function (key) {
-            if (key.key == 'Enter') {
-                search();
-            }
+function generateMenu() {
+    let menu = document.querySelector('nav.main-menu ul');
+    menu.innerHTML = '';
 
-        })
+    let items = [
+        {href: 'index.html', text: 'Товар'},
+        {href: '', text: 'Контакты'},
+        {href: '', text: 'Доставка'},
+        {href: '', text: 'Акции'},
+        {href: '', text: 'О нас'},
+    ];
+
+    for(let i = 0; i<items.length; i++) {
+        let link = document.createElement('a');
+        link.innerText = items[i].text;
+        link.href = items[i].href;
+        if(items[i].href == '') {
+            link.addEventListener('click', notReadyAlert);
+        }
+
+        let menuItem = document.createElement('li');
+        menuItem.appendChild(link);
+
+        menu.appendChild(menuItem);
     }
+
+}
+
+function loaded() {
+    let searchbox = document.getElementById('search');
+    searchbox.addEventListener('keydown', function (key) {
+        if (key.key == 'Enter') {
+            search();
+        }
+
+    });
+
+    generateMenu();
 }
